ef1e9d792c3251aa300be865971c9beb961c9b12
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-11-23 11:17:30 +0000

    [Add] edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'books/new'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index', as: 'index_book'
   get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
