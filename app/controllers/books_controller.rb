56a2ab40f07580703408dfddb5e90b0922b66eb4
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-11-24 13:22:48 +0000

    [Add] Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,15 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    .destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
