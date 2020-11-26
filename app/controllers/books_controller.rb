b379ad48092a57bfff55652d43211bc3e8131244
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-11-26 08:26:52 +0000

    [Update] サクセスメッセージの表示機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
       @books = Book.all
       render action: :index
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
     else
       render action: :edit
     end
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
